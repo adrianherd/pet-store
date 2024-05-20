b24ea15e1e96d82112736e772d0b7deb7ac81c92
Adrian Hernandez <adherna2@cisco.com>
2024-05-20 00:41:00 -0500

    Make landing page index for root

--- apps/pet-store/src/app/routes.tsx
+++ apps/pet-store/src/app/routes.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, createRoutesFromElements, Route } from 'react-rout
 import { BasicLayout } from '@pet-store/shared/layouts/basic-layout/ui';
 import { Login } from '@pet-store/pet-store/pages/login/feat';
 import { PetList } from '@pet-store/pet-store/pages/pet-list/feat';
+import { Landing } from '@pet-store/pet-store/pages/landing/feat';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<BasicLayout />}>
+      <Route index element={<Landing />}/>
       <Route path="login" element={<Login />} />
       <Route path="pet-list" element={<PetList />} />
     </Route>
