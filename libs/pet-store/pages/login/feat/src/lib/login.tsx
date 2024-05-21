66433c23b424694c84af216d1311f6db944296a9
Adrian Hernandez <adherna2@cisco.com>
2024-05-20 21:28:29 -0500

    add some styling to login page

--- libs/pet-store/pages/login/feat/src/lib/login.tsx
+++ libs/pet-store/pages/login/feat/src/lib/login.tsx
@@ -1,14 +1,18 @@
 import styles from './login.module.scss';
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import CardHeader from '@mui/material/CardHeader';
+import Container from '@mui/material/Container'
 import LinearProgress from '@mui/material/LinearProgress';
+import Stack from '@mui/material/Stack'
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IS_AUTHENTICATED_TOKEN } from '@pet-store/shared/core/user/util';
+
 
 export function Login() {
   const [email, setEmail] = useState('');
@@ -34,48 +38,55 @@ export function Login() {
     navigate('/pet-list')
   }
   return (
-    <Card className={styles.card}>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        void loginHandler();
-      }}>
-        <CardHeader title="Pet Store SSO" />
-        <CardContent>
-          <div>
-            <TextField
-              required
-              id="user-email"
-              name="email"
-              label="Email"
-              variant="outlined"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-            />
-          </div>
-          <div>
-            <TextField
-              required
-              id="user-password"
-              name="password"
-              label="Password"
-              type="password"
-              variant="outlined"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-            />
-          </div>
-        </CardContent>
-        <CardActions>
-          <div>
-            <Button disabled={loading} onClick={loginHandler} variant="contained">
-              Login
-            </Button>
-            {loading && <LinearProgress />}
-          </div>
-        </CardActions>
-      </form>
-    </Card>
-  );
+    <Box width={1} height={'100vh'} justifyContent={'center'} alignContent={'center'} >
+      <Container maxWidth={'sm'}>
+        <Card className={styles.card}>
+          <form onSubmit={(event) => {
+            event.preventDefault();
+            void loginHandler();
+          }}>
+            <CardHeader title="Pet Store SSO" />
+            <CardContent>
+              <Stack spacing={2}>
+                <div>
+                  <TextField
+                    required
+                    id="user-email"
+                    name="email"
+                    label="Email"
+                    variant="outlined"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                  />
+                </div>
+                <div>
+                  <TextField
+                    required
+                    id="user-password"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                  />
+                </div>
+              </Stack>
+            </CardContent>
+            <CardActions>
+              <Box justifyContent={'center'} width={1}>
+                <Button disabled={loading} onClick={loginHandler} variant="contained">
+                  Login
+                </Button>
+                <Box mb={2}></Box>
+                {loading && <LinearProgress />}
+              </Box>
+            </CardActions>
+          </form>
+        </Card>
+      </Container>
+    </Box>
+  )
 }
 
 export default Login;
