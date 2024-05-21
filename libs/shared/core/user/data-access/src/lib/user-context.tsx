da5f1347f1ad4545e05277bd44bd568ad556c014
Adrian Hernandez <adherna2@cisco.com>
2024-05-21 02:02:06 -0500

    fixed session storage reset bug

--- libs/shared/core/user/data-access/src/lib/user-context.tsx
+++ libs/shared/core/user/data-access/src/lib/user-context.tsx
@@ -2,18 +2,23 @@ import { createContext, ReactNode, useEffect, useState } from 'react';
 import { IS_AUTHENTICATED_TOKEN } from '@pet-store/shared/core/user/util';
 
 export interface AuthContext {
-  isAuthenticated: boolean;
+  isAuthenticated: boolean
   setIsAuthenticated: (isAuthenticated: boolean) => void;
 }
 
 export const UserAuthContext = createContext<AuthContext>({
+  // Session storage is used to set initial value in provider below
   isAuthenticated: false,
-  // This function is a stubbed default value and replaced with a useState set dispatch in provider below
-  setIsAuthenticated: (authenticated: boolean) => authenticated
+  // This function is replaced with a useState set dispatch in provider below
+  setIsAuthenticated: () => {/* Do nothing */}
 });
 
+/* Use this provider to avoid duplication of isAuth logic */
 export const UserAuthContextProvider = ({ children }: { children: ReactNode}) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    () => sessionStorage.getItem(IS_AUTHENTICATED_TOKEN) === 'true'
+  );
+
   useEffect(() => {
     sessionStorage.setItem(IS_AUTHENTICATED_TOKEN, `${isAuthenticated}`)
   }, [isAuthenticated]);
