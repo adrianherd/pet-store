b79566ec1a82b81b9846f3f0a91d876f3dac9f39
Adrian Hernandez <adherna2@cisco.com>
2024-05-22 12:09:07 -0500

    login page unit test skeleton

--- libs/pet-store/pages/login/feat/src/lib/login.spec.tsx
+++ libs/pet-store/pages/login/feat/src/lib/login.spec.tsx
@@ -2,9 +2,16 @@ import { render } from '@testing-library/react';
 
 import Login from './login';
 
+
 describe('Login', () => {
-  it('should render successfully', () => {
+  it('has a login title', () => {
     const { baseElement } = render(<Login />);
     expect(baseElement).toBeTruthy();
   });
+  it('has a email field')
+  it('has a password field')
+  it('has a login button')
+  it('makes a login request on login button click')
+  it('shows a loading bar while login request is being made')
+  it('triggers a user login event on successful login request')
 });
