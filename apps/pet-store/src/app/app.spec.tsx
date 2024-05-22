7077f35f950368939c2a81baccac0624a7e126ef
Adrian Hernandez <adherna2@cisco.com>
2024-05-22 11:29:03 -0500

    App.spec.tsx unit test

--- apps/pet-store/src/app/app.spec.tsx
+++ apps/pet-store/src/app/app.spec.tsx
@@ -1,15 +1,33 @@
 import { render } from '@testing-library/react';
-
+import { vi } from 'vitest';
 import App from './app';
+import { UserAuthContextProvider } from '@pet-store/shared/core/user/data-access';
+import { RouterProvider } from 'react-router-dom';
+
+vi.mock('@pet-store/shared/core/user/data-access', () => ({
+  UserAuthContextProvider: vi.fn(({ children }) => children),
+}));
+
+vi.mock('react-router-dom', async (importOriginal) => {
+  return {
+    ...await importOriginal<typeof import('react-router-dom')>(),
+    RouterProvider: vi.fn(),
+  }
+})
 
 describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
+  beforeEach(() => {
+    // Clear all mock calls before each test
+    vi.clearAllMocks()
   });
 
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome pet-store/gi)).toBeTruthy();
+  it('provides a UserAuthContextProvider', () => {
+    render(<App />);
+    expect(vi.mocked(UserAuthContextProvider)).toHaveBeenCalled();
+  });
+
+  it('provides a RouterContext', () => {
+    render(<App />);
+    expect(vi.mocked(RouterProvider)).toHaveBeenCalled();
   });
 });
--- apps/pet-store/src/app/app.tsx
+++ apps/pet-store/src/app/app.tsx
@@ -2,8 +2,6 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { UserAuthContextProvider } from '@pet-store/shared/core/user/data-access';
 
-
-
 export function App() {
   return (
     <UserAuthContextProvider>
